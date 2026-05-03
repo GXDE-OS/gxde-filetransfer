07dff3bfb57dcc9fc556e4940966af6d2e256dbf
momen <vmomenv@gmail.com>
2026-05-03 13:04:55 +0800

    Adjust port field and DTK dependencies

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -205,6 +205,7 @@ QWidget *MainWindow::createConnectionBar()
     m_portSpin = new QSpinBox(bar);
     m_portSpin->setRange(0, 65535);
     m_portSpin->setSpecialValueText(tr("Auto"));
+    m_portSpin->setMinimumWidth(96);
     m_userEdit = new QLineEdit(bar);
     m_userEdit->setPlaceholderText(tr("User"));
     m_passwordEdit = new QLineEdit(bar);

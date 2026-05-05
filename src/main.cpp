faf9f7c7d7c1b5322d5880cf316ac661196ae834
momen <vmomenv@gmail.com>
2026-05-05 20:58:01 +0800

    Release 1.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     DApplication app(argc, argv);
     app.setOrganizationName("gxde");
     app.setApplicationName("gxde-filetransfer");
-    app.setApplicationVersion("1.0.0");
+    app.setApplicationVersion("1.0.1");
     const QIcon appIcon(QStringLiteral(":/icons/gxde-filetransfer.svg"));
     app.setWindowIcon(appIcon);
     app.setProductIcon(appIcon);
